d391a9d4456303276b3be580c0c74810db7f9a2e
Jean-Paul <imurhero2@users.noreply.github.com>
2018-02-07 13:23:33 -0500

    Movement, Waypoints
    Changed player movement and created a temporary waypoint system for enemy movement.

---
+++ RPG Defense/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 10f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(h * speed * Time.deltaTime, v * speed * Time.deltaTime);
+		}
+	}
